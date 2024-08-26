68c0e2dddfe85aacc04e322656a7be549424c1c3
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-08-26 08:23:30 +0300

    Start project from scratch

--- spellbook/src/essence.rs
+++
@@ -1,7 +0,0 @@
-pub struct Essence(String);
-
-impl Essence {
-    pub fn new(essence: String) -> Self {
-        Self(essence)
-    }
-}
--- spellbook/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod name;
-mod essence;
-mod spell;
-mod catalog;
-
-pub use spell::{Spell, SpellComponents};
-pub use catalog::Catalog;
