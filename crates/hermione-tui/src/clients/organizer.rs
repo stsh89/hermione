edb964e2c13578121a98cef997bb072f433e97e6
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-09-08 09:33:37 +0300

    Remove wasted comments

--- crates/hermione-tui/src/clients/organizer.rs
+++ crates/hermione-tui/src/clients/organizer.rs
@@ -107,32 +107,6 @@ impl Load for Inner {
         });
 
         Ok(organizer)
-
-        // let mut organizer = Organizer::empty();
-        // let mut workspace = Workspace::new(WorkspaceName::new("Hermione".to_string()));
-        // let mut command = Command::new(Program::new("cargo fmt".to_string()));
-        // command.set_name(CommandName::new("Format project".to_string()));
-        // workspace.add_command(command);
-
-        // let mut command = Command::new(Program::new("cargo clippy".to_string()));
-        // command.set_name(CommandName::new("Lint project".to_string()));
-        // workspace.add_command(command);
-        // organizer.add_workspace(workspace);
-
-        // let mut workspace = Workspace::new(WorkspaceName::new("General".to_string()));
-        // let command = Command::new(Program::new("Get-ChildItem".to_string()));
-        // workspace.add_command(command);
-        // organizer.add_workspace(workspace);
-
-        // let mut workspace = Workspace::new(WorkspaceName::new("Vulkan tutorial".to_string()));
-        // let mut command = Command::new(Program::new(
-        //     r#"C:\VulkanSDK\1.3.290.0\Bin\glslc.exe .\shaders\shader.frag -o .\shaders\frag.spv"#
-        //         .to_string(),
-        // ));
-        // command.set_name(CommandName::new("Compile shader fragment".to_string()));
-        // workspace.add_command(command);
-        // organizer.add_workspace(workspace);
-        // Ok(organizer)
     }
 }
 
