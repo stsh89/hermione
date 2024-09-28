82a5a2dbae2bf6add85fc59d85c5dc1f11639b59
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-09-28 16:31:49 +0300

    Fix executor

--- crates/hermione-eyes/src/clients/executor.rs
+++ crates/hermione-eyes/src/clients/executor.rs
@@ -1,33 +1,30 @@
-use crate::types::{Command, Result};
-use std::{io::Write, process::Stdio};
+use crate::types::Result;
 
 pub struct Client<'a> {
-    pub command: &'a Command,
+    pub program: &'a str,
     pub location: &'a str,
+    pub execute_immediately: bool,
 }
 
 impl<'a> Client<'a> {
     pub fn execute(&self) -> Result<()> {
-        let program = format!(
-            "wt pwsh -Command {{cd {}; {}; Read-Host \"Press any key\"}}",
-            self.location, self.command.program
-        );
-        let mut cmd = std::process::Command::new("PowerShell");
-        cmd.stdin(Stdio::piped());
-        cmd.stdout(Stdio::piped());
-        cmd.stderr(Stdio::piped());
-        let mut process = cmd.spawn()?;
-        let stdin = process
-            .stdin
-            .as_mut()
-            .ok_or(anyhow::anyhow!("stdin access failure"))?;
-        writeln!(stdin, "{}", program)?;
-        process.wait_with_output()?;
+        let command = if self.execute_immediately {
+            self.program
+        } else {
+            &format!("Set-Clipboard '{}'", &self.program)
+        };
+
+        std::process::Command::new("wt")
+            .args([
+                "pwsh",
+                "-NoExit",
+                "-WorkingDirectory",
+                self.location,
+                "-Command",
+                command,
+            ])
+            .spawn()?;
 
         Ok(())
-    }
-
-    pub fn new(command: &'a Command, location: &'a str) -> Self {
-        Self { command, location }
     }
 }
