974a74118198dbf5612768a3e7b6327dd89d3562
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-09-30 14:32:55 +0300

    Remove wasted code

--- crates/hermione-eyes/src/router/powershell/mod.rs
+++ crates/hermione-eyes/src/router/powershell/mod.rs
@@ -18,17 +18,3 @@ pub struct CopyToClipboardParameters {
 pub struct StartWindowsTerminalParameters {
     pub working_directory: Option<String>,
 }
-
-macro_rules! from_parameters {
-    ($action:ident, $parameters:ident) => {
-        impl From<$parameters> for Router {
-            fn from(parameters: $parameters) -> Self {
-                Router::$action(parameters)
-            }
-        }
-    };
-}
-
-from_parameters!(ExecuteCommand, ExecuteCommandParameters);
-from_parameters!(CopyToClipboard, CopyToClipboardParameters);
-from_parameters!(StartWindowsTerminal, StartWindowsTerminalParameters);
