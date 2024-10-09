f89a78f8c8c45070b194045d2fc8ea41f41ccf0c
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-10-09 14:56:31 +0300

    [notion-sync] Update CLI commands
    Finalize ability to create and delete settings file.

--- crates/hermione-notion-sync/src/main.rs
+++ crates/hermione-notion-sync/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use hermione_notion::QueryDatabaseParameters;
 use serde::{Deserialize, Serialize};
 use std::{fs::File, path::Path};
 
@@ -26,35 +27,72 @@ struct Settings {
     workspaces_page_id: String,
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let app_path = hermione_terminal_directory::path()?;
 
     let cli = Cli::parse();
 
     let path = app_path.join(SETTINGS_FILE_NAME);
 
-    match cli.command {
-        Commands::CreateSettingsFile => create_settings_file(&path),
+    let result = match cli.command {
+        Commands::CreateSettingsFile => create_settings_file(&path).await,
         Commands::DeleteSettingsFile => delete_settings_file(&path),
+    };
+
+    if let Err(error) = result {
+        eprintln!("{error:?}");
     }
+
+    Ok(())
 }
 
-fn create_settings_file(settings_file_path: &Path) -> Result<()> {
+async fn create_settings_file(settings_file_path: &Path) -> Result<()> {
     if settings_file_path.try_exists()? {
         return Err(Error::msg("Settings file already exists"));
     }
 
+    clear_screen();
     let api_key = read_stdin("Enter your Notion API key: ")?;
+
+    clear_screen();
     let workspaces_page_id = read_stdin("Enter your Notion workspaces page ID: ")?;
+
     let settings = Settings {
         api_key,
         workspaces_page_id,
     };
 
+    clear_screen();
+    println!("Settings verification started...");
+
+    let client = hermione_notion::Client::new(hermione_notion::ClientParameters {
+        timeout: std::time::Duration::from_secs(10),
+        api_key: Some(settings.api_key.clone()),
+        base_url_override: None,
+    })?;
+
+    client
+        .query_database(QueryDatabaseParameters {
+            api_key_override: None,
+            page_size: 1,
+            start_cursor: None,
+            database_id: &settings.workspaces_page_id,
+        })
+        .await?;
+
+    println!("Settings verified!");
+
     let file = File::create(settings_file_path)?;
     serde_json::to_writer_pretty(file, &settings)?;
 
+    println!("Settings file created: {}", settings_file_path.display());
+
     Ok(())
+}
+
+fn clear_screen() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
 
 fn delete_settings_file(settings_file_path: &Path) -> Result<()> {
