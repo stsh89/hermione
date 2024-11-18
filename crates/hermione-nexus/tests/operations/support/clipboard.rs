05d4fbe21bd2c9a1f01e65ee3065b13926ef162b
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-11-18 11:11:45 +0200

    [nexus] Minor tests update

--- crates/hermione-nexus/tests/operations/support/clipboard.rs
+++ crates/hermione-nexus/tests/operations/support/clipboard.rs
@@ -14,26 +14,26 @@ impl MockClipboard {
     pub fn empty() -> Self {
         Self::default()
     }
-
-    pub fn set_content(&self, text: &str) -> Result<()> {
-        let mut content = self.content.write().map_err(|_err| {
-            Error::Clipboard(eyre!(
-                "Shared memory blocked for writing, can proceeed with copying"
-            ))
-        })?;
-
-        *content = Some(text.to_string());
-
-        Ok(())
-    }
 }
 
 impl ClipboardService for MockClipboard {}
 
 impl CopyCommandToClipboard for MockClipboard {
     fn copy_command_to_clipboard(&self, text: &str) -> Result<()> {
-        self.set_content(text)?;
+        set_clipboard_content(self, text)
+            .map_err(|err| Error::Clipboard(err.wrap_err("Can't copy command to clipboard")))?;
 
         Ok(())
     }
+}
+
+fn set_clipboard_content(clipboard: &MockClipboard, text: &str) -> eyre::Result<()> {
+    let mut content = clipboard
+        .content
+        .write()
+        .map_err(|_err| eyre!("Clipboard memory write access failure"))?;
+
+    *content = Some(text.to_string());
+
+    Ok(())
 }
