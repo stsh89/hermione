55e8cd2ae5c9298575f187f6c23f4be7e6ceff04
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-11-27 20:33:40 +0200

    [nexus] Add operation to delete commands by workspace ids

---
+++ crates/hermione-nexus/src/operations/delete_commands.rs
@@ -0,0 +1,39 @@
+use crate::{
+    definitions::WorkspaceId,
+    services::{DeleteWorkspaceCommands, StorageService},
+    Result,
+};
+
+pub struct DeleteCommandsOperation<'a, DWC>
+where
+    DWC: StorageService,
+{
+    pub delete_workspace_commands: &'a DWC,
+}
+
+pub struct DeleteCommandsParameters {
+    pub delete_attribute: CommandsDeleteAttribute,
+}
+
+pub enum CommandsDeleteAttribute {
+    WorkspaceId(WorkspaceId),
+}
+
+impl<'a, DWC> DeleteCommandsOperation<'a, DWC>
+where
+    DWC: DeleteWorkspaceCommands,
+{
+    pub fn delete_workspace_commands(&self, id: &WorkspaceId) -> Result<()> {
+        self.delete_workspace_commands.delete_workspace_commands(id)
+    }
+
+    pub fn execute(&self, parameters: DeleteCommandsParameters) -> Result<()> {
+        let DeleteCommandsParameters { delete_attribute } = parameters;
+
+        use CommandsDeleteAttribute as Attr;
+
+        match delete_attribute {
+            Attr::WorkspaceId(workspace_id) => self.delete_workspace_commands(&workspace_id),
+        }
+    }
+}
